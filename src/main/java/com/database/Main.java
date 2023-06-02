ac6e58d4b362ccb403e4006a1a738ce40b2211c8
pedro-cfg <pedro.guimaraes@gmail.com>
2023-06-02 20:43:01 -0300

    pequenas correções

--- src/main/java/com/database/Main.java
+++ src/main/java/com/database/Main.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)
-    {   
-        System.out.println("Hello World");
-        
+    {           
         SQLManager manager = new SQLManager();
-        //FileReader fr = new FileReader();
-
-        //fr.readTable("pessoa");
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Insert a SQL statement: ");
-        String statement = scanner.nextLine();
-        manager.processStatement(statement);
+        boolean fim = false;
+
+        while(!fim)
+        {
+            System.out.println("Insert a SQL statement: ");
+            String statement = scanner.nextLine();
+            manager.processStatement(statement);
+        }
 
         scanner.close();
     }
